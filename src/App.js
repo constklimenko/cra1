4fff67d859718d938d2243bd56340407b6dd2fbc
narcom <narcomacer@gmail.com>
2020-05-28 23:13:26 +0500

    2

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
 import React from 'react';
 
 import './App.css';
-import Header from './Header.js';
-import Technologies from './Technologies.js'
-import Footer from './Footer';
-
-
-
 
 const App = () => {
   return (
-    <div className="App">
-      < Header />
-      <Technologies />
-      <Footer />
+    <div className="app-wrapper">
+      <header className="header">
+        <img className="logo" src="https://cdn.dribbble.com/users/182238/screenshots/2383317/lion2.jpg" alt="" />
+      </header>
+      <nav className="nav"></nav>
+      <main className="main">
+        <img src="http://www.coolwebmasters.com/uploads/posts/2012-08/1346142320_widescreen-wallpaper-01.jpg" alt="" srcset="" />
+        <div>
+          ava + description
+        </div>
+
+        <div>
+
+          posts
+          
+          <div>
+            new post
+          </div>
+        </div>
+      </main>
 
     </div>
   );
