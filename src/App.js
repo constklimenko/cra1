7a3bd19d470a78e1093dd9d8dc7a7d6bb9e97dec
narcom <narcomacer@gmail.com>
2020-05-29 12:01:43 +0500

    поддержка less

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Header from './components/Header'
 import Navbar from './components/Navbar'
 import Profile from './components/Profile'
-import './App.css';
+import './css/App.css';
 
 const App = () => {
   return (
